60a067c359350a6f24499d25e254586a0c709cb6
Danny Tu <dannytu09@hotmail.com>
2021-10-17 00:08:03 -0400

    Update P2StepDefinitions.java
    tried to do my part

--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P2StepDefinitions.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P2StepDefinitions.java
@@ -8,7 +8,12 @@ import io.cucumber.java.en.When;
 
 //test for commmit github
 
+
 public class P2StepDefinitions {
+	
+	
+	
+	
   @Given("the following ClimbSafe system exists: \\(p2)")
   public void the_following_climb_safe_system_exists_p2(io.cucumber.datatable.DataTable dataTable) {
     // Write code here that turns the phrase above into concrete actions
@@ -22,7 +27,7 @@ public class P2StepDefinitions {
   }
 
   @Given("the following equipment exists in the system: \\(p2)")
-  public void the_following_equipment_exists_in_the_system_p2(
+  public void the_following_equipment_exists_in_the_system_p2( // Danny 
       io.cucumber.datatable.DataTable dataTable) {
     // Write code here that turns the phrase above into concrete actions
     // For automatic transformation, change DataTable to one of
@@ -31,48 +36,73 @@ public class P2StepDefinitions {
     // Double, Byte, Short, Long, BigInteger or BigDecimal.
     //
     // For other transformations you can register a DataTableType.
+	 List<Map<String, String>> rows = dataTable.asMaps(); //not sure to understand here
+	 
+	 for (var row : rows) {
+		 
+		 String name = row.get("name");
+		 int weight = Integer.parseInt(row.get("weight"));
+		 int pricePerWeek = Interger.parseInt(row.get("pricePerWeek"));
+		 
+		 climbsafe.addEquipement(row.get("weight")), climbsafe.addEquipement(row.get("pricePerWeek"))
+		 
+		 	
+		
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	
+	 
+	
+	  
+	  
+	  
     throw new io.cucumber.java.PendingException();
   }
 
   @When("the administrator attempts to add an equipment bundle with name {string}, discount {string}, items {string}, and quantities {string} \\(p2)")
-  public void the_administrator_attempts_to_add_an_equipment_bundle_with_name_discount_items_and_quantities_p2(
+  public void the_administrator_attempts_to_add_an_equipment_bundle_with_name_discount_items_and_quantities_p2( //Salim will do
       String string, String string2, String string3, String string4) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
   @Then("the number of equipment bundles in the system shall be {string} \\(p2)")
-  public void the_number_of_equipment_bundles_in_the_system_shall_be_p2(String string) {
+  public void the_number_of_equipment_bundles_in_the_system_shall_be_p2(String string) {//Arturo
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
   @Then("the equipment bundle {string} shall exist in the system \\(p2)")
-  public void the_equipment_bundle_shall_exist_in_the_system_p2(String string) {
+  public void the_equipment_bundle_shall_exist_in_the_system_p2(String string) {//Noah
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
-  @Then("the equipment bundle {string} shall contain the items {string} with quantities {string} \\(p2)")
+  @Then("the equipment bundle {string} shall contain the items {string} with quantities {string} \\(p2)") //Karen
   public void the_equipment_bundle_shall_contain_the_items_with_quantities_p2(String string,
       String string2, String string3) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
-  @Then("the equipment bundle {string} shall have a discount of {string} \\(p2)")
+  @Then("the equipment bundle {string} shall have a discount of {string} \\(p2)")//Everyone can do it
   public void the_equipment_bundle_shall_have_a_discount_of_p2(String string, String string2) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
-  @Then("the error {string} shall be raised \\(p2)")
+  @Then("the error {string} shall be raised \\(p2)")//Peini
   public void the_error_shall_be_raised_p2(String string) {
     // Write code here that turns the phrase above into concrete actions
     throw new io.cucumber.java.PendingException();
   }
 
-  @Given("the following equipment bundles exist in the system: \\(p2)")
+  @Given("the following equipment bundles exist in the system: \\(p2)")//Peini
   public void the_following_equipment_bundles_exist_in_the_system_p2(
       io.cucumber.datatable.DataTable dataTable) {
     // Write code here that turns the phrase above into concrete actions
