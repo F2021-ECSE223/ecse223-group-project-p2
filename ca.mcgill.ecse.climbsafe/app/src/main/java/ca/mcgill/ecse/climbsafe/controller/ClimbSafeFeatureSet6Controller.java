5e47196d9d00b5d88ae3ea4377247fc67d44c1f9
PeiniCheng <90059889+PeiniCheng@users.noreply.github.com>
2021-10-18 22:08:37 -0400

    commit1

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet6Controller.java
@@ -1,14 +1,63 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.Administrator;
+import ca.mcgill.ecse.climbsafe.model.Assignment;
+import ca.mcgill.ecse.climbsafe.model.BookableItem;
+import ca.mcgill.ecse.climbsafe.model.BookedItem;
+import ca.mcgill.ecse.climbsafe.model.BundleItem;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.Equipment;
+import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
+import ca.mcgill.ecse.climbsafe.model.Guide;
+import ca.mcgill.ecse.climbsafe.model.Hotel;
+import ca.mcgill.ecse.climbsafe.model.Member;
+import ca.mcgill.ecse.climbsafe.model.NamedUser;
+import ca.mcgill.ecse.climbsafe.model.User;
 public class ClimbSafeFeatureSet6Controller {
+	private static ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
 
-  public static void deleteEquipment(String name) throws InvalidInputException {}
-
+  public static void deleteEquipment(String name) throws InvalidInputException {
+  List<Equipment> equipments = climbSafe.getEquipment();
+  for (Equipment e : equipments) {
+	  if (e.getName() == name){
+		  e.delete();
+		  break;
+	  }
+  }
+  }
   // this method does not need to be implemented by a team with five team members
-  public static void deleteEquipmentBundle(String name) {}
-
-  public static List<TOAssignment> getAssignments() {}
-
+  public static void deleteEquipmentBundle(String name) {
+  List<EquipmentBundle> bundles = climbSafe.getBundles();
+  for (EquipmentBundle b : bundles) {
+	  if (b.getName() == name){
+		  b.delete();
+		  break;
+	  }
+  }
+  }
+  public static List<TOAssignment> getAssignments() {
+  var assignments = new ArrayList<TOAssignment>();
+  for (var assignment : climbSafe.getAssignments()) {
+	 int week = assignment.getEndWeek() - assignment.getStartWeek();
+	 int totalCostForEquipmentPerWeek=0;
+	  List<BookedItem> bookedItems = assignment.getMember().getBookedItems();
+	  for(BookedItem booked : bookedItems) {
+		  BookableItem i = booked.getItem();
+		  if (i instanceof Equipment) {
+			  totalCostForEquipmentPerWeek += ((Equipment)i).getPricePerWeek();
+		  }else {
+			  for(BundleItem b : ((EquipmentBundle)i).getBundleItems()) {
+			  totalCostForEquipmentPerWeek += (b.getEquipment().getPricePerWeek()) * (b.getQuantity());
+			  }
+		  }
+	  }
+    assignments.add(new TOAssignment(assignment.getMember().getEmail(), assignment.getMember().getName(),
+    		assignment.getGuide().getEmail(), assignment.getGuide().getName(),assignment.getHotel().getName(), assignment.getStartWeek(),
+    		assignment.getEndWeek(), (assignment.getClimbSafe().getPriceOfGuidePerWeek()) * week, totalCostForEquipmentPerWeek * week ));
+  }
+  return assignments;
+}
 }
--- ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P2StepDefinitions.java
+++ ca.mcgill.ecse.climbsafe/app/src/test/java/ca/mcgill/ecse/climbsafe/features/P2StepDefinitions.java
@@ -1,40 +1,118 @@
 package ca.mcgill.ecse.climbsafe.features;
 
+import io.cucumber.java.After;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
+import static org.junit.Assert.assertEquals;
+
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.controller.ClimbSafeFeatureSet5Controller;
+import ca.mcgill.ecse.climbsafe.controller.InvalidInputException;
+import ca.mcgill.ecse.climbsafe.model.Administrator;
+import ca.mcgill.ecse.climbsafe.model.Assignment;
+import ca.mcgill.ecse.climbsafe.model.BookableItem;
+import ca.mcgill.ecse.climbsafe.model.BookedItem;
+import ca.mcgill.ecse.climbsafe.model.BundleItem;
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+import ca.mcgill.ecse.climbsafe.model.Equipment;
+import ca.mcgill.ecse.climbsafe.model.EquipmentBundle;
+import ca.mcgill.ecse.climbsafe.model.Guide;
+import ca.mcgill.ecse.climbsafe.model.Hotel;
+import ca.mcgill.ecse.climbsafe.model.Member;
+import ca.mcgill.ecse.climbsafe.model.NamedUser;
+import ca.mcgill.ecse.climbsafe.model.User;
 public class P2StepDefinitions {
+	
+	private ClimbSafe climbSafe;
+	String error;
+	
+	
   @Given("the following ClimbSafe system exists: \\(p2)")
   public void the_following_climb_safe_system_exists_p2(io.cucumber.datatable.DataTable dataTable) {
     // Write code here that turns the phrase above into concrete actions
+	  climbSafe=ClimbSafeApplication.getClimbSafe();
+	  List<Map<String, String>> rows = dataTable.asMaps();
+	  for (var row : rows) {
+	  climbSafe.setStartDate(Date.valueOf(row.get("startDate")));
+	  climbSafe.setNrWeeks(Integer.parseInt(row.get("nrWeeks")));
+	  climbSafe.setPriceOfGuidePerWeek(Integer.parseInt(row.get("priceOfGuidePerWeek")));
+	  }
     // For automatic transformation, change DataTable to one of
     // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
     // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
     // Double, Byte, Short, Long, BigInteger or BigDecimal.
     //
     // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    //throw new io.cucumber.java.PendingException();
   }
 
   @Given("the following equipment exists in the system: \\(p2)")
   public void the_following_equipment_exists_in_the_system_p2(
       io.cucumber.datatable.DataTable dataTable) {
     // Write code here that turns the phrase above into concrete actions
+	  List<Map<String, String>> rows = dataTable.asMaps(); //not sure to understand here
+		 
+		 for (var row : rows) {
+			 
+			 String name = row.get("name");
+			 int weight = Integer.parseInt(row.get("weight"));
+			 int pricePerWeek = Integer.parseInt(row.get("pricePerWeek"));
+			 
+			 climbSafe.addEquipment(name,weight,pricePerWeek);
+			 
+			 	
+			
+		 }
+		 
+    // For other transformations you can register a DataTableType.
+    //throw new io.cucumber.java.PendingException();
     // For automatic transformation, change DataTable to one of
     // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
     // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
     // Double, Byte, Short, Long, BigInteger or BigDecimal.
     //
     // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
   }
-
+  
+//salim
   @When("the administrator attempts to add an equipment bundle with name {string}, discount {string}, items {string}, and quantities {string} \\(p2)")
   public void the_administrator_attempts_to_add_an_equipment_bundle_with_name_discount_items_and_quantities_p2(
       String string, String string2, String string3, String string4) {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	    // Write code here that turns the phrase above into concrete actions
+	      //convert the strings to actual useful stuff 
+	  try{ String name = string;
+	      int discount = Integer.parseInt(string2);
+	      if (discount<0) {
+			  error = "Discount must be at least 0 ";
+		  }
+		  if(discount>100) {
+			  error = "Discount must be no more than 100 ";
+		  }
+		  if(name.isEmpty()) {
+			  error = "Equipment bundle name cannot be empty ";
+		  }
+	      ArrayList<String> itemList = new ArrayList<String>();
+		  ArrayList<Integer> quantityList = new ArrayList<Integer>();
+	      for(String item: string3.split(",")){
+			   itemList.add(item);
+			}
+		  for(String quantity: string4.split(",")){
+			   quantityList.add(Integer.parseInt(quantity));
+			}
+	      ClimbSafeFeatureSet5Controller.addEquipmentBundle(name, discount,itemList,quantityList );
+	      }catch(InvalidInputException e) {
+	    	  error=e.getMessage();
+	      }
+	      
+    //throw new io.cucumber.java.PendingException();
   }
 
   @Then("the number of equipment bundles in the system shall be {string} \\(p2)")
@@ -62,23 +140,56 @@ public class P2StepDefinitions {
     throw new io.cucumber.java.PendingException();
   }
 
+  //@author Peini Cheng
   @Then("the error {string} shall be raised \\(p2)")
   public void the_error_shall_be_raised_p2(String string) {
     // Write code here that turns the phrase above into concrete actions
-    throw new io.cucumber.java.PendingException();
+	  assertEquals(string,error);
+   // throw new io.cucumber.java.PendingException();
   }
 
+  //@author Peini Cheng
   @Given("the following equipment bundles exist in the system: \\(p2)")
   public void the_following_equipment_bundles_exist_in_the_system_p2(
       io.cucumber.datatable.DataTable dataTable) {
     // Write code here that turns the phrase above into concrete actions
+	  List<Map<String, String>> rows = dataTable.asMaps();
+	  for (var row : rows) {
+	  EquipmentBundle bundle = climbSafe.addBundle(row.get("name"), Integer.parseInt(row.get("discount")));
+	  String items = row.get("items");
+	  ArrayList<String> itemList = new ArrayList<String>();
+	  String quantities = row.get("quantities");
+	  ArrayList<String> quantityList = new ArrayList<String>();
+	  for(String item: items.split(",")){
+		   itemList.add(item);
+		}
+	  for(String quantity: quantities.split(",")){
+		   quantityList.add(quantity);
+		}
+	  for (int i=0;i<itemList.size();i++) {
+		  List<Equipment>equipments = climbSafe.getEquipment();
+		  for(Equipment e : equipments) {
+			  if (e.getName() == itemList.get(i)) {
+				  BundleItem bundleItem = bundle.addBundleItem(Integer.parseInt(quantityList.get(i)), climbSafe, e);  
+				  bundle.addBundleItem(bundleItem);
+			  }
+		  }
+	  }
+	  
+	  climbSafe.addBundle(bundle);
+	  }
     // For automatic transformation, change DataTable to one of
     // E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
     // Map<K, List<V>>. E,K,V must be a String, Integer, Float,
     // Double, Byte, Short, Long, BigInteger or BigDecimal.
     //
     // For other transformations you can register a DataTableType.
-    throw new io.cucumber.java.PendingException();
+    //throw new io.cucumber.java.PendingException();
+  }
+  
+  @After
+  public void tearDown() {
+    climbSafe.delete();
   }
 
 }
