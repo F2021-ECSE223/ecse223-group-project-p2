0ea1591cd3f9b29677d8f8c934187c3a370d5518
salimei <salim.benchekroun@mail.mcgill.ca>
2021-10-23 15:22:04 -0400

    working on controller set 1

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeFeatureSet1Controller.java
@@ -2,12 +2,21 @@ package ca.mcgill.ecse.climbsafe.controller;
 
 import java.sql.Date;
 
+import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
+
 public class ClimbSafeFeatureSet1Controller {
 
   public static void setup(Date startDate, int nrWeeks, int priceOfGuidePerWeek)
-      throws InvalidInputException {}
+      throws InvalidInputException {
+	  if ( nrWeeks < 0 || priceOfGuidePerWeek < 0) {
+		  throw new InvalidInputException("nrWeeks and priceOfGuide must not be negative");
+	  }
+	  ClimbSafe new_instance = new ClimbSafe(startDate, nrWeeks, priceOfGuidePerWeek);
+  }
 
-  public static void deleteMember(String email) {}
+  public static void deleteMember(String email) {
+	  
+  }
 
   public static void deleteGuide(String email) {}
 
