d44f4096a14549a2ae6a4b79c5b10019ffe44cba
salimei <salim.benchekroun@mail.mcgill.ca>
2021-11-09 13:08:26 -0500

    working on intiate assignments

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,16 +1,50 @@
 package ca.mcgill.ecse.climbsafe.controller;
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
+import ca.mcgill.ecse.climbsafe.model.Assignment;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
 import ca.mcgill.ecse.climbsafe.model.Equipment;
 import ca.mcgill.ecse.climbsafe.model.Guide;
 import ca.mcgill.ecse.climbsafe.model.Member;
 public class AssignmentController {
  //4,5 
-  public static void InitiateAssignment() {
-    //salim work
+  public static void InitiateAssignment() { //initiate all assignments
+	  
+	  ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
+	  Assignment assignment = climbSafe.getAssignment(0);
+ 	  
+	  int i = 0;
+	  int j = 0;
+	  
+	  while ( i <  climbSafe.getGuides().size() ) {
+		 while ( j < climbSafe.getMembers().size() ){
+			 
+			 
+			 Member m = climbSafe.getMember(j);
+			 if ( m.getGuideRequired() == true ) { // if member needs a guide 
+				 
+			 }
+			 
+			 assignment = climbSafe.getAssignment(j);
+			 int start = assignment.getStartWeek();
+			 int end = assignment.getEndWeek();
+			 
+			 
+			 if ( climbSafe.getGuide(i).hasAssignments() == false ) {  // if guide available (doesn't have a member during that period) 
+			 
+			 }
+			 
+			 
+			 if ( assignment.hasGuide() == false  ) { //if the member is unassigned and requested a guide, then assign 
+				Assignment a1 = new Assignment(start, end, climbSafe.getMember(j), climbSafe); 
+			 }
+			 
+			 j++;
+		 }
+		 i++; 
+	  }
   }
  //6
-  public static void StartTrip(int week) {
+  public static void StartTrip(int week) { //starts for all members startdate == week
 
   }
   
