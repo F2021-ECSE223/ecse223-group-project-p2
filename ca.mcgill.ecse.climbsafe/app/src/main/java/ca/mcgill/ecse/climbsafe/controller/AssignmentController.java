1b9e3d05e0e0ad460dc4f3d098c45c3f0189b4b3
PeiniCheng <90059889+PeiniCheng@users.noreply.github.com>
2021-11-10 19:54:39 -0500

    Added JavaDoc except for initiateAssignment()

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/AssignmentController.java
@@ -1,5 +1,6 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 import ca.mcgill.ecse.climbsafe.controller.InvalidInputException;
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
@@ -11,83 +12,73 @@ import ca.mcgill.ecse.climbsafe.model.Member;
 
 public class AssignmentController {
   // 4,5
-  public static void InitiateAssignment() {
-    ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
-    int totalWeek = climbSafe.getNrWeeks();
-    int indexMember = 0;
-    for(Guide aGuide: climbSafe.getGuides()) {
-      while(indexMember<climbSafe.getMembers().size()) {
-        Member m = climbSafe.getMember(indexMember);
-        if (m.getGuideRequired() == false && (!m.hasAssignment())) { // if member doesn't want a
-          // guide
-          Assignment a = new Assignment(1, m.getNrWeeks(), climbSafe.getMember(indexMember), climbSafe);
-        }else if(Utility.isAviliable(totalWeek, aGuide)) {
-          int startWeek=0;
-          int endWeek = 0;
-          if(aGuide.getAssignments().size() == 0) {
-            startWeek = 1;
-            endWeek = m.getNrWeeks()-1;
-            Assignment a = new Assignment(startWeek, endWeek, climbSafe.getMember(indexMember), climbSafe);
-            a.setGuide(aGuide); // add the guide to the assignment
-          }else {
-            startWeek = aGuide.getAssignment(aGuide.getAssignments().size()).getStartWeek()+1;
-            endWeek = startWeek+m.getNrWeeks()-1;
-            Assignment a = new Assignment(startWeek, endWeek, climbSafe.getMember(indexMember), climbSafe);
-            a.setGuide(aGuide);
-          }
-        }else if(Utility.isAviliable(totalWeek, aGuide) == false) {
-          indexMember++;
-          break;
-        }
-        indexMember++;
-      }
-    }
-  }
-  public static void InitiateAssignment2() { // initiate all assignments
+  public static void initiateAssignment() throws InvalidInputException { // initiate all assignments
 
     ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
-
+    ArrayList<Assignment> allAss = new ArrayList<Assignment>();
    int memberIndex = 0;
    for (int i = 0; i < climbSafe.getGuides().size();i++) {
-      System.out.println(climbSafe.getGuides().size());
-      Guide g = climbSafe.getGuide(i);
-      while (memberIndex < climbSafe.getMembers().size()) {
-        Member m = climbSafe.getMember(memberIndex); // for convenience
-        int nbrWeeks = m.getNrWeeks(); // number of weeks member wants to climb
-        int totalWeeks = climbSafe.getNrWeeks(); // number of weeks in climbing season
-        int start = 0;
-        int end = 0;
-        // Assignment assignment = climbSafe.getAssignment(j);
-        // test if guide is available during that time
-        // 1.calculate number of weeks guide is busy
-        int k = 0;
-        int sum = 0;
-        while (k < g.getAssignments().size()) {
-          sum += g.getAssignments().get(k).getMember().getNrWeeks();
-          k++;
-        }
-        // 2. Is ( nbrWeeks_available >= nbrWeeks_member)?
-        // ie. (totalWeeks - sum) >= nbrWeeks?
-        if (m.getGuideRequired() == false && (!m.hasAssignment())) { // if member doesn't want a
-                                                                     // guide
-          Assignment a = new Assignment(0, m.getNrWeeks(), climbSafe.getMember(memberIndex), climbSafe);
-        }
-        if ((totalWeeks - sum) >= nbrWeeks) {
-          start = sum + 1;
-          end = start + nbrWeeks - 1;
-          if (m.getGuideRequired() == true && (!m.hasAssignment())) { // if member needs a guide,                                                                     // but has no guide yet
-                Assignment a = new Assignment(start, end, climbSafe.getMember(memberIndex), climbSafe);
+      Guide currentGuide = climbSafe.getGuide(i);
+      for (Member currentMember: climbSafe.getMembers()) {
+           
+            
+            if ( currentMember.hasAssignment() == true ) {
+                memberIndex ++;
+                continue;
+            }
+            
+            if (!currentMember.getGuideRequired()) {
+                Assignment a = new Assignment(1, currentMember.getNrWeeks(), currentMember, climbSafe);
+                climbSafe.addAssignment(a);
+                allAss.add(a);
+                currentMember.getAssignment().setTestStatus("Assigned");
+                memberIndex ++;
+                continue;
+            }
+        
+            int memberWeeks = currentMember.getNrWeeks(); // number of weeks member wants to climb
+            int totalWeeks = climbSafe.getNrWeeks(); // number of weeks in climbing season
+            int start = 0;
+            int end = 0;
+            // Assignment assignment = climbSafe.getAssignment(j);
+            // test if guide is available during that time
+            // 1.calculate number of weeks guide is busy
+    
+            int busyWeeks = 0;
+            for (int k = 0; k < currentGuide.getAssignments().size(); k++) {
+              busyWeeks += currentGuide.getAssignments().get(k).getMember().getNrWeeks();
+            }
+            // 2. Is ( nbrWeeks_available >= nbrWeeks_member)?
+            // ie. (totalWeeks - sum) >= nbrWeeks?
+            if ((totalWeeks - busyWeeks) >= memberWeeks) {
+              start = busyWeeks + 1;
+              end = start + memberWeeks - 1;
+              // if member needs a guide,                                                                     // but has no guide yet
+                Assignment a = new Assignment(start, end, currentMember, climbSafe);
                 a.setGuide(climbSafe.getGuide(i)); // add the guide to the assignment
-          }
-          memberIndex ++;
-          break;
-        }
-        memberIndex ++;
+                climbSafe.addAssignment(a);
+                currentMember.getAssignment().setTestStatus("Assigned");
+                allAss.add(a);
+                memberIndex ++;
+                continue;
+    
+             }
+            memberIndex ++;
       }
     }
+   
+   for (Member m : climbSafe.getMembers() ) {
+       if ( m.hasAssignment() == false ) {
+           throw new InvalidInputException("Assignments could not be completed for all members");
+       }
+   }
   }
 
-  // 6
+  /**
+   * used by Administrater to start all trips for a specific week
+   * @author Runge (Karen) Fu
+   * @param  week week number
+   */
   public static void StartTrip(int week) throws InvalidInputException {
     String error = null;
     ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
@@ -102,7 +93,11 @@ public class AssignmentController {
         }
     }
   }
-
+  /**
+   * used by Administrater to finish a member's trip
+   * @author Peini Cheng
+   * @param  email email of the required member
+   */
   public static void finishTrip(String email) throws InvalidInputException {
     String error = null;
     try {
@@ -113,7 +108,11 @@ public class AssignmentController {
       throw new InvalidInputException(error);
     }
   }
-
+  /**
+   * used by Administrater to cancel a member's trip
+   * @author Peini Cheng
+   * @param  email email of the required member
+   */
   public static void cancelTrip(String email) throws InvalidInputException {
     String error = null;
     try {
@@ -124,7 +123,12 @@ public class AssignmentController {
       throw new InvalidInputException(error);
     }
   }
-
+  /**
+   * used by Administrater to confirm payment for a member
+   * @author Peini Cheng
+   * @param  email email of the required member
+   * @param  code authorization code
+   */
   public static void confirmPayment(String email, String code) throws InvalidInputException {
     String error = null;
     try {
@@ -136,7 +140,11 @@ public class AssignmentController {
     }
 
   }
-
+  /**
+   * helper method to find member with email.
+   * @author Peini Cheng
+   * @param  email email of the required member
+   */
   private static Member findMember(String email) throws InvalidInputException {
     ClimbSafe climbSafe = ClimbSafeApplication.getClimbSafe();
     Member foundMember = null;
