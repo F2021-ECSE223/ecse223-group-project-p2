af176dcb67a4a3df46e4bf91bc464b2eaface400
PeiniCheng <90059889+PeiniCheng@users.noreply.github.com>
2021-11-30 18:19:29 -0500

    backup

---
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/javafx/controllers/ProcessAssignmentPageController.java
@@ -0,0 +1,64 @@
+package ca.mcgill.ecse.climbsafe.javafx.controllers;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.TextField;
+import static ca.mcgill.ecse.climbsafe.javafx.controllers.ViewUtils.callController;
+import ca.mcgill.ecse.climbsafe.controller.AssignmentController;
+import javafx.event.ActionEvent;
+
+public class ProcessAssignmentPageController {
+	@FXML
+	private TextField startTextField;
+	@FXML
+	private TextField cancelTextField;
+	@FXML
+	private TextField FinishTextField;
+
+	// Event Listener on Button.onAction
+	@FXML
+	public void startClicked(ActionEvent event) {
+	  if(startTextField.getText().trim().isEmpty()) {
+	    ViewUtils.showError("Please input a valid week number");
+	  }
+	  else {
+	  Integer week=Integer.parseInt(startTextField.getText());
+      try{callController(() -> AssignmentController.StartTrip(week));
+      }catch(RuntimeException e) {
+        ViewUtils.showError(e.getMessage());
+      }
+	  }
+      startTextField.clear();
+	}
+	// Event Listener on Button.onAction
+	@FXML
+	public void cancelClicked(ActionEvent event) {
+	  if(cancelTextField.getText().trim().isEmpty()) {
+        ViewUtils.showError("Please input a valid email");
+      }
+      else {
+	  String email=cancelTextField.getText();
+      try{callController(() -> AssignmentController.cancelTrip(email));
+      }catch(RuntimeException e) {
+        ViewUtils.showError(e.getMessage());
+      }
+      }
+      cancelTextField.clear();
+	}
+	  
+	// Event Listener on Button.onAction
+	@FXML
+	public void finishClicked(ActionEvent event) {
+	  if(FinishTextField.getText().trim().isEmpty()) {
+        ViewUtils.showError("Please input a valid email");
+      }
+      else {
+	  String email=FinishTextField.getText();
+      try{callController(() -> AssignmentController.finishTrip(email));
+      }catch(RuntimeException e) {
+        ViewUtils.showError(e.getMessage());
+      }
+      }
+      FinishTextField.clear();
+	}
+}
