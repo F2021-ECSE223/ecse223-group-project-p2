3691e1c406de661f283c28277c3cd27679d3351e
PeiniCheng <90059889+PeiniCheng@users.noreply.github.com>
2021-12-02 20:36:12 -0500

    update

--- ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeBonusFeaturesController.java
+++ ca.mcgill.ecse.climbsafe/app/src/main/java/ca/mcgill/ecse/climbsafe/controller/ClimbSafeBonusFeaturesController.java
@@ -1,7 +1,7 @@
 package ca.mcgill.ecse.climbsafe.controller;
 
 import java.util.ArrayList;
-
+import java.util.List;
 import ca.mcgill.ecse.climbsafe.application.ClimbSafeApplication;
 import ca.mcgill.ecse.climbsafe.model.ClimbSafe;
 import ca.mcgill.ecse.climbsafe.model.Guide;
@@ -22,40 +22,25 @@ public class ClimbSafeBonusFeaturesController {
 	/**
 	 * @author Salim Benchekroun 
 	 */
-
 	//forget about this method
-	public static void deleteAllGuides() { //deletes all guides in the system 
-
-		ClimbSafe inst = ClimbSafeApplication.getClimbSafe();
-		for (Guide g : inst.getGuides()) {
-			g.delete();
-		}   
-		ClimbSafePersistence.save();
-	}
-
-	//forget about this method
-	public static void banAllMembers() { //bans all members in the system 
-
-		ClimbSafe inst = ClimbSafeApplication.getClimbSafe();
-		for (Member m : inst.getMembers()) {
-			m.banMember();
-		}   
-		ClimbSafePersistence.save();
-	}
-
+    //public static List<double> getNumericStats() {
+     // List<Integer> numberList = new ArrayList<Integer>();
+     // numberList.add(numberList);
+    //}
 	//returns distribution of member with and without guide
 	@SuppressWarnings("unchecked")
 	public static ObservableList<Data> getMemberStats() {
-		int wG = 0; //number of members with a guide
-		int nG = 0;//number of members without a guide 
 		ClimbSafe inst = ClimbSafeApplication.getClimbSafe();
-
+		int wG = 0; //number of members with a guide
+	    int nG = 0;//number of members without a guide 
 		for (Member m : inst.getMembers()) { //count the number of member in each category 
 			if ( m.isGuideRequired() == true) {
 				wG++;
+				System.out.println("wiht guide!");
 			}
 			if ( m.isGuideRequired() == false) {
 				nG++;
+				System.out.println("no guide!");
 			}
 		}
 
@@ -63,16 +48,17 @@ public class ClimbSafeBonusFeaturesController {
 		ObservableList<PieChart.Data> b = FXCollections.observableArrayList();
 		if ( inst.getMembers().size() == 0) { // if no members, return 0
 			b.add(new PieChart.Data("no member in list", 0));
+			System.out.println("no member!");
 			return b;
 		}
 		//if there are members in the list, return the percentages 
 
 		//calculate percentage
-		int percent_wG = wG/(inst.getMembers().size()) * 100;
-		int percent_nG = 100 - percent_wG;
+		double percent_wG = ((double)((double)wG/(double)(inst.getMembers().size()))) * 100;
+		double percent_nG = 100 - percent_wG;
 
-		b.add(new PieChart.Data("with Guide " + percent_wG + "%", percent_wG));
-		b.add(new PieChart.Data("no Guide " + percent_nG + "%", percent_nG));
+		b.add(new PieChart.Data("with Guide " + (int)percent_wG + "%", (int)percent_wG));
+		b.add(new PieChart.Data("no Guide " + (int)percent_nG + "%", (int)percent_nG));
 
 		return b;
 	}
